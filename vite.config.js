10cef6bd9769b4bcc62b8410fa073d68482cc55b
raunchard <reinhard.zach@gmail.com>
2023-03-05 07:50:54 +0300

    serve gzip in case no brotli support

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-	plugins: [react(), viteCompression({ algorithm: "brotliCompress" })],
+	plugins: [react(), viteCompression({ algorithm: "brotliCompress" }), viteCompression({ algorithm: "gzip" })],
 });

d570d0fd70ad6e4a2e8191236bcfdd0b55bed9d4
raunchard <reinhard.zach@gmail.com>
2023-03-05 08:42:02 +0300

    fix ascii art aria

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,13 +3,13 @@ import { image, text } from "../assets/AsciiArt";
 
 export default function Logo() {
 	return (
-		<div className="logo">
+		<figure className="logo" role="img" aria-label="codegpt ninja logo ascii art" >
 			<pre className="logo-img">
 				<Typewriter text={image} duration={2000} />
 			</pre>
 			<pre className="logo-text">
 				<Typewriter text={text} duration={2000} />
 			</pre>
-		</div>
+		</figure>
 	);
 }

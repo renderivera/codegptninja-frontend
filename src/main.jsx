32608aa6727948af297b8d7818c83cb0ca571933
raunchard <reinhard.zach@gmail.com>
2023-03-08 08:31:36 +0300

    remove unused component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App";
 import buildStore from "./store";
 
-disableReactDevTools();
 const store = buildStore();
 
 createRoot(document.getElementById("root")).render(
